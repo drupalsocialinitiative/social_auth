25d56f1682f736ff72939ce1d8c4a7d1425def5a
Getulio Sánchez <valentin2507@gmail.com>
2017-01-21 13:26:17 -0300

    Issue #2779641 by slivorezka, nikathone: Provide method to store user's picture

--- src/SocialAuthUserManager.php
+++ src/SocialAuthUserManager.php
@@ -2,17 +2,23 @@
 
 namespace Drupal\social_auth;
 
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\social_auth\Event\SocialAuthUserEvent;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\Core\Transliteration\PhpTransliteration;
+use Drupal\Core\Utility\Token;
 use Drupal\social_auth\Event\SocialAuthEvents;
+use Drupal\social_auth\Event\SocialAuthUserEvent;
 use Drupal\social_auth\Event\SocialAuthUserFieldsEvent;
 use Drupal\user\Entity\User;
 use Drupal\user\UserInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Entity\EntityStorageException;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Contains all logic that is related to Drupal user management.
@@ -24,6 +30,11 @@ class SocialAuthUserManager {
   protected $loggerFactory;
   protected $eventDispatcher;
   protected $entityTypeManager;
+  protected $entityFieldManager;
+  protected $token;
+  protected $stringTranslation;
+  protected $transliteration;
+  protected $languageManager;
 
   /**
    * Constructor.
@@ -33,15 +44,28 @@ class SocialAuthUserManager {
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
    *   Used for logging errors.
    * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
-   *   Used for dispatching events to other modules.
+   *   Used for dispatching social auth events.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Used for loading and creating Drupal user objects.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   Used for access Drupal user field definitions.
+   * @param \Drupal\Core\Utility\Token $token
+   *   Used for token support in Drupal user picture directory.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   Used for translating strings in UI messages.
+   * @param \Drupal\Core\Transliteration\PhpTransliteration $transliteration
+   *   Used for user picture directory and file transliteration.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, LoggerChannelFactoryInterface $logger_factory, EventDispatcherInterface $event_dispatcher, EntityTypeManagerInterface $entity_type_manager) {
-    $this->configFactory = $config_factory;
-    $this->loggerFactory = $logger_factory;
-    $this->eventDispatcher = $event_dispatcher;
-    $this->entityTypeManager = $entity_type_manager;
+  public function __construct(ConfigFactoryInterface $config_factory, LoggerChannelFactoryInterface $logger_factory, EventDispatcherInterface $event_dispatcher, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, Token $token, TranslationInterface $string_translation, PhpTransliteration $transliteration, LanguageManagerInterface $language_manager) {
+    $this->configFactory      = $config_factory;
+    $this->loggerFactory      = $logger_factory;
+    $this->eventDispatcher    = $event_dispatcher;
+    $this->entityTypeManager  = $entity_type_manager;
+    $this->entityFieldManager = $entity_field_manager;
+    $this->token              = $token;
+    $this->stringTranslation  = $string_translation;
+    $this->transliteration    = $transliteration;
+    $this->languageManager    = $language_manager;
   }
 
   /**
@@ -77,7 +101,7 @@ class SocialAuthUserManager {
    * Create a new user account.
    *
    * @param string $name
-   *   User's name on Facebook.
+   *   User's name on Provider.
    * @param string $email
    *   User's email address.
    * @param string $plugin_id
@@ -102,11 +126,15 @@ class SocialAuthUserManager {
         ->get($plugin_id)
         ->warning('Failed to create user. User registration is disabled in Drupal account settings. Name: @name, email: @email.', array('@name' => $name, '@email' => $email));
 
+      $this->drupalSetMessage($this->t('Only existing users can log in with Facebook. Contact system administrator.'), 'error');
       return FALSE;
     }
 
+    // Get the current UI language.
+    $langcode = $this->languageManager->getCurrentLanguage()->getId();
+
     // Initializes the user fields.
-    $fields = $this->getUserFields($name, $email, $plugin_id);
+    $fields = $this->getUserFields($name, $email, $langcode, $plugin_id);
 
     // Create new user account.
     /** @var \Drupal\user\Entity\User $new_user */
@@ -114,13 +142,27 @@ class SocialAuthUserManager {
       ->getStorage('user')
       ->create($fields);
 
+    // Validate the new user.
+    $violations = $new_user->validate();
+    if (count($violations) > 0) {
+      $msg = $violations[0]->getMessage();
+      $this->drupalSetMessage($this->t('Creation of user account failed: @message', array('@message' => $msg)), 'error');
+      $this->loggerFactory
+        ->get($plugin_id)
+        ->error('Could not create new user: @message', array('@message' => $msg));
+      return FALSE;
+    }
+
     // Try to save the new user account.
     try {
       $new_user->save();
 
       $this->loggerFactory
         ->get($plugin_id)
-        ->notice('New user created. Username @username, UID: @uid', array('@username' => $new_user->getAccountName(), '@uid' => $new_user->id()));
+        ->notice('New user created. Username @username, UID: @uid', [
+          '@username' => $new_user->getAccountName(),
+          '@uid' => $new_user->id(),
+        ]);
 
       // Dipatches SocialAuthEvents::USER_CREATED event.
       $event = new SocialAuthUserEvent($new_user, $plugin_id);
@@ -131,7 +173,7 @@ class SocialAuthUserManager {
     catch (EntityStorageException $ex) {
       $this->loggerFactory
         ->get($plugin_id)
-        ->error('Could not create new user. Exception: @message', array('@message' => $ex->getMessage()));
+        ->error('Could not create new user. Exception: @message', ['@message' => $ex->getMessage()]);
     }
 
     return FALSE;
@@ -187,25 +229,36 @@ class SocialAuthUserManager {
   /**
    * Ensures that Drupal usernames will be unique.
    *
-   * Drupal usernames will be generated so that the user's full name on Facebook
+   * Drupal usernames will be generated so that the user's full name on Provider
    * will become user's Drupal username. This method will check if the username
    * is already used and appends a number until it finds the first available
    * username.
    *
    * @param string $name
-   *   User's full name on Facebook.
+   *   User's full name on provider.
    *
    * @return string
-   *   Unique username
+   *   Unique drupal username.
    */
   protected function generateUniqueUsername($name) {
-    $base = trim($name);
+    $max_length = 60;
+    $name = Unicode::substr($name, 0, $max_length);
+
+    // Add a trailing number if needed to make username unique.
+    $base = $name;
     $i = 1;
     $candidate = $base;
     while ($this->loadUserByProperty('name', $candidate)) {
       $i++;
-      $candidate = $base . " " . $i;
+      // Calculate max length for $base and truncate if needed.
+      $max_length_base = $max_length - strlen((string) $i) - 1;
+      $base = Unicode::substr($base, 0, $max_length_base);
+      $candidate = $base . ' ' . $i;
     }
+
+    // Trim leading and trailing whitespace.
+    $candidate = trim($candidate);
+
     return $candidate;
   }
 
@@ -224,6 +277,155 @@ class SocialAuthUserManager {
     }
 
     return 0;
+  }
+
+  /**
+   * Downloads and sets user profile picture.
+   *
+   * @param User $drupal_user
+   *   User object to update the profile picture for.
+   * @param string $picture_url
+   *   Absolute URL where the picture will be downloaded from.
+   * @param string $id
+   *   User's ID.
+   * @param string $plugin_id
+   *   Social Auth plugin id.
+   *
+   * @return bool
+   *   True if picture was successfully set.
+   *   False otherwise.
+   */
+  public function setProfilePic(User $drupal_user, $picture_url, $id, $plugin_id = 'social_auth') {
+    // Try to download the profile picture and add it to user fields.
+    if ($this->userPictureEnabled()) {
+      if ($file = $this->downloadProfilePic($picture_url, $id, $plugin_id)) {
+        $drupal_user->set('user_picture', $file->id());
+        $drupal_user->save();
+        return TRUE;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Downloads the profile picture to Drupal filesystem.
+   *
+   * @param string $picture_url
+   *   Absolute URL where to download the profile picture.
+   * @param string $id
+   *   Social network ID of the user.
+   * @param string $plugin_id
+   *   Social Auth plugin id.
+   *
+   * @return \Drupal\file\FileInterface|false
+   *   FileInterface object if file was succesfully downloaded
+   *   False otherwise
+   */
+  protected function downloadProfilePic($picture_url, $id, $plugin_id = 'social_auth') {
+    // Make sure that we have everything we need.
+    if (!$picture_url || !$id || !$plugin_id) {
+      return FALSE;
+    }
+
+    // Determine target directory.
+    $scheme = $this->configFactory->get('system.file')->get('default_scheme');
+    $file_directory = $this->getPictureDirectory();
+
+    if (!$file_directory) {
+      return FALSE;
+    }
+    $directory = $scheme . '://' . $file_directory;
+
+    // Replace tokens.
+    $directory = $this->token->replace($directory);
+
+    // Transliterate directory name.
+    $directory = $this->transliteration->transliterate($directory, 'en', '_', 50);
+
+    if (!$this->filePrepareDirectory($directory, 1)) {
+      $this->loggerFactory
+        ->get($plugin_id)
+        ->error('Could not save @plugin_id\'s provider profile picture. Directory is not writable: @directory', [
+          '@directory' => $directory,
+          '@provider' => $plugin_id,
+        ]);
+      return FALSE;
+    }
+
+    // Generate filename and transliterate.
+    $filename = $this->transliteration->transliterate($plugin_id . '_' . $id . '.jpg', 'en', '_', 50);
+
+    $destination = $directory . DIRECTORY_SEPARATOR . $filename;
+
+    // Download the picture to local filesystem.
+    if (!$file = $this->systemRetrieveFile($picture_url, $destination, TRUE, 1)) {
+      $this->loggerFactory
+        ->get($plugin_id)
+        ->error('Could not download @plugin_id\'s provider profile picture from url: @url', [
+          '@url' => $picture_url,
+          '@plugin_id' => $plugin_id,
+        ]);
+
+      return FALSE;
+    }
+
+    return $file;
+  }
+
+  /**
+   * Returns whether this site supports the default user picture feature.
+   *
+   * We use this method instead of the procedural user_pictures_enabled()
+   * so that we can unit test our own methods.
+   *
+   * @return bool
+   *   True if user pictures are enabled
+   *   False otherwise
+   */
+  protected function userPictureEnabled() {
+    $field_definitions = $this->entityFieldManager->getFieldDefinitions('user', 'user');
+    return isset($field_definitions['user_picture']);
+  }
+
+  /**
+   * Returns picture directory if site supports the user picture feature.
+   *
+   * @return string|bool
+   *   Directory for user pictures if site supports user picture feature.
+   *   False otherwise.
+   */
+  protected function getPictureDirectory() {
+    $field_definitions = $this->entityFieldManager->getFieldDefinitions('user', 'user');
+    if (isset($field_definitions['user_picture'])) {
+      return $field_definitions['user_picture']->getSetting('file_directory');
+    }
+    return FALSE;
+  }
+
+  /**
+   * Wrapper for file_prepare_directory.
+   *
+   * We need to wrap the legacy procedural Drupal API functions so that we are
+   * not using them directly in our own methods. This way we can unit test our
+   * own methods.
+   *
+   * @see file_prepare_directory
+   */
+  protected function filePrepareDirectory(&$directory, $options) {
+    return file_prepare_directory($directory, $options);
+  }
+
+  /**
+   * Wrapper for system_retrieve_file.
+   *
+   * We need to wrap the legacy procedural Drupal API functions so that we are
+   * not using them directly in our own methods. This way we can unit test our
+   * own methods.
+   *
+   * @see system_retrieve_file
+   */
+  protected function systemRetrieveFile($url, $destination, $managed, $replace) {
+    return system_retrieve_file($url, $destination, $managed, $replace);
   }
 
   /**
@@ -262,14 +464,18 @@ class SocialAuthUserManager {
    * Returns an array of fields to initialize the creation of the user.
    *
    * @param string $name
-   *   User's name on Facebook.
+   *   User's name on Provider.
    * @param string $email
    *   User's email address.
+   * @param string $langcode
+   *   The current UI language.
+   * @param string $plugin_id
+   *   The plugin id.
    *
    * @return array
    *   Fields to initialize for the user creation.
    */
-  protected function getUserFields($name, $email, $plugin_id = 'social_auth') {
+  protected function getUserFields($name, $email, $langcode, $plugin_id = 'social_auth') {
     // - Password can be very long since the user doesn't see this.
     $fields = [
       'name' => $this->generateUniqueUsername($name),
@@ -277,6 +483,9 @@ class SocialAuthUserManager {
       'init' => $email,
       'pass' => $this->userPassword(32),
       'status' => $this->getNewUserStatus(),
+      'langcode' => $langcode,
+      'preferred_langcode' => $langcode,
+      'preferred_admin_langcode' => $langcode,
     ];
 
     // Dispatches SocialAuthEvents::USER_FIELDS, so that other modules can
