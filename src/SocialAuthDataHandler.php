9da4d292d8bd41317d240138497a658b1a47b2a3
Himanshu Dixit <hudixt@gmail.com>
2017-08-23 11:57:03 +0530

    SocialDataHandler to extend SocialApiDataHandler

--- src/SocialAuthDataHandler.php
+++ src/SocialAuthDataHandler.php
@@ -1,16 +1,13 @@
 <?php
 
 namespace Drupal\social_auth;
-
+use Drupal\social_api\SocialApiDataHandler;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * Variables are written to and read from session via this class.
  */
-class SocialAuthDataHandler {
-  protected $session;
-  protected $sessionPrefix;
-
+class SocialAuthDataHandler extends SocialApiDataHandler {
   /**
    * Constructor.
    *
@@ -18,52 +15,6 @@ class SocialAuthDataHandler {
    *   Used for reading data from and writing data to session.
    */
   public function __construct(SessionInterface $session) {
-    $this->session = $session;
+    parent::__construct($session);
   }
-
-  /**
-   * Gets a session variable by key.
-   *
-   * @param string $key
-   *   The session variable key.
-   *
-   * @return mixed
-   *   The session variable value.
-   */
-  public function get($key) {
-    return $this->session->get($this->getSessionPrefix() . $key);
-  }
-
-  /**
-   * Sets a new session variable.
-   *
-   * @param string $key
-   *   The session variable key.
-   * @param mixed $value
-   *   The session variable value.
-   */
-  public function set($key, $value) {
-    $this->session->set($this->getSessionPrefix() . $key, $value);
-  }
-
-  /**
-   * Gets the session prefix for the data handler.
-   *
-   * @return string
-   *   The session prefix.
-   */
-  public function getSessionPrefix() {
-    return $this->sessionPrefix;
-  }
-
-  /**
-   * Sets the session prefix for the data handler.
-   *
-   * @param string $prefix
-   *   The session prefix.
-   */
-  public function setSessionPrefix($prefix) {
-    $this->sessionPrefix = $prefix . '_';
-  }
-
 }
