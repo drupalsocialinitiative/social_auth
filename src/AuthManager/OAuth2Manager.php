3c55bfc7b7644c0388bce525364d40c247e2ee9d
Getulio Sánchez <valentin2507@gmail.com>
2018-01-27 11:20:11 -0500

    Issue #2940041: Expand OAuth2Manager

--- src/AuthManager/OAuth2Manager.php
+++ src/AuthManager/OAuth2Manager.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\social_auth\AuthManager;
 
+use Drupal\Core\Config\Config;
 use Drupal\social_api\AuthManager\OAuth2Manager as BaseOAuth2Manager;
 
 /**
@@ -11,5 +12,62 @@ use Drupal\social_api\AuthManager\OAuth2Manager as BaseOAuth2Manager;
  */
 abstract class OAuth2Manager extends BaseOAuth2Manager implements OAuth2ManagerInterface {
 
-  // Do nothing.
+  /**
+   * Social Auth implementer settings.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $settings;
+
+  /**
+   * The scopes to be requested.
+   *
+   * @var string
+   */
+  protected $scopes;
+
+  /**
+   * The end points to be requested.
+   *
+   * @var string
+   */
+  protected $endPoints;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Config\Config $settings
+   *   The implementer settings.
+   */
+  public function __construct(Config $settings) {
+    $this->settings = $settings;
+    $this->endPoints = $this->scopes = FALSE;
+  }
+
+  /**
+   * Gets the scopes defined in the settings form.
+   *
+   * @return string
+   *   Data points separated by comma.
+   */
+  public function getScopes() {
+    if ($this->scopes === FALSE) {
+      $this->scopes = $this->settings->get('scopes');
+    }
+    return $this->scopes;
+  }
+
+  /**
+   * Gets the API endpoints to be requested.
+   *
+   * @return string
+   *   API endpoints separated in different lines.
+   */
+  public function getEndPoints() {
+    if ($this->endPoints === FALSE) {
+      $this->endPoints = $this->settings->get('endpoints');
+    }
+    return $this->endPoints;
+  }
+
 }
--- src/AuthManager/OAuth2ManagerInterface.php
+++ src/AuthManager/OAuth2ManagerInterface.php
@@ -11,4 +11,39 @@ use Drupal\social_api\AuthManager\OAuth2ManagerInterface as BaseOAuth2ManagerInt
  */
 interface OAuth2ManagerInterface extends BaseOAuth2ManagerInterface {
 
+  /**
+   * Request data from the declared endpoints.
+   *
+   * @return array
+   *   The extra details gotten from provider.
+   */
+  public function getExtraDetails();
+
+  /**
+   * Request and end point.
+   *
+   * @param string $path
+   *   The path or url to request.
+   *
+   * @return array|mixed
+   *   Data returned by provider.
+   */
+  public function requestEndPoint($path);
+
+  /**
+   * Gets the scopes defined in the settings form.
+   *
+   * @return string
+   *   Data points separated by comma.
+   */
+  public function getScopes();
+
+  /**
+   * Gets the API endpoints to be requested.
+   *
+   * @return string
+   *   API endpoints separated in different lines.
+   */
+  public function getEndPoints();
+
 }
