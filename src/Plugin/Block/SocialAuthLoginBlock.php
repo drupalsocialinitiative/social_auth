687db5cf790928644c69a9a4821a6c34cbc51eee
Getulio Sánchez <valentin2507@gmail.com>
2018-05-03 10:32:43 -0400

    Fix coding standards

--- src/Plugin/Block/SocialAuthLoginBlock.php
+++ src/Plugin/Block/SocialAuthLoginBlock.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\social_auth\Plugin\Block;
 
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -49,10 +47,10 @@ class SocialAuthLoginBlock extends BlockBase implements ContainerFactoryPluginIn
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-    $configuration,
-    $plugin_id,
-    $plugin_definition,
-    $container->get('config.factory')->get('social_auth.settings')
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')->get('social_auth.settings')
     );
   }
 
