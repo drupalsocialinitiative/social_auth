6d22cb069c06b00b280c2cf8503b0beba0e012e6
neerajp99 <neerajp1999@gmail.com>
2019-06-06 23:43:33 +0530

    Add tests for class FailedAuthenticationEvent

--- tests/src/Unit/EventTest.php
+++ tests/src/Unit/EventTest.php
@@ -5,6 +5,8 @@ use PHPUnit\Framework\TestCase;
 use Drupal\social_auth\Event\BeforeRedirectEvent;
 use Drupal\social_auth\SocialAuthDataHandler;
 use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\social_auth\Event\FailedAuthenticationEvent;
 
 
 
@@ -12,6 +14,8 @@ class EventTest extends UnitTestCase {
   protected $data_handler;
   protected $pluginId = 'drupal123';
   protected $destination = 'drupal123';
+  protected $error = "error404";
+  protected $response = 'drupal';
 
 
   /**
@@ -61,6 +65,35 @@ class EventTest extends UnitTestCase {
     $this->assertSame('drupal123', $collection->getPluginId());
     $this->assertSame('drupal123', $collection->getDestination());
   }
+
+  /**
+   * test for class FailedAuthenticationEvent
+   */
+
+   public function testFailedAuthenticationEvent () {
+     $this->data_handler = $this->createMock(SocialAuthDataHandler::class);
+     $collection = $this->getMockBuilder('Drupal\social_auth\Event\FailedAuthenticationEvent')
+                        ->setConstructorArgs(array($this->data_handler, $this->pluginId, $this->error))
+                        ->setMethods(array('getDataHandler', 'getPluginId', 'getError'))
+                        ->getMock();
+     // $reflector = new ReflectionClass( 'Drupal\social_auth\Event\FailedAuthenticationEvent' );
+     // $method = $reflector->getMethod('setResponse');
+     // $method->setAccessible( true );
+     // $method->invokeArgs($collection, array($responses));
+     $responses = $this->createMock(RedirectResponse::class);
+     $collection->setResponse($responses);
+     $this->assertEquals($responses, $collection->getResponse());
+     // var_dump($collection->hasResponse());
+     $this->assertEquals(true, $collection->hasResponse());
+     $collection->method('getError')
+                ->willReturn($this->error);
+     $collection->method('getPluginId')
+                ->willReturn($this->pluginId);
+     $collection->method('getDataHandler')
+                ->willReturn($this->data_handler);
+     $this->assertEquals('error404', $collection->getError());
+     $this->assertEquals('drupal123', $collection->getPluginId());
+   }
 
 }
 
