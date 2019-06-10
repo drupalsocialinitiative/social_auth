f08797c42cdcf386d872124ba44ca8023f8234df
neerajp99 <neerajp1999@gmail.com>
2019-06-11 05:14:26 +0530

    Edit tests for SocialAuthDataHandler

--- tests/src/Unit/SocialAuthDataHandlerTest.php
+++ tests/src/Unit/SocialAuthDataHandlerTest.php
@@ -8,12 +8,27 @@ use Drupal\social_api\SocialApiDataHandler;
 class SocialAuthDataHandlerTest extends UnitTestCase {
 
   /**
+   * __construct function
+   */
+  public function __construct() {
+       parent::__construct();
+   }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
    * tests for class SocialAuthDataHandler
    */
 
   public function testSocialAuthDataHandler () {
 
-    $collection = $this->getMockBuilder('Drupal\social_auth\SocialAuthDataHandler')
+    $collection = $this->getMockBuilder(SocialAuthDataHandler::class)
                           ->disableOriginalConstructor()
                           ->getMock();
     $this->assertTrue($collection instanceof SocialAuthDataHandler);
