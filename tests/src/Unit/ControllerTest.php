e6eecdb855334caddfe5e38661e70b2ca897be50
neerajp99 <neerajp1999@gmail.com>
2019-06-16 19:35:28 +0530

    Edit tests for Controller

--- tests/src/Unit/ControllerTest.php
+++ tests/src/Unit/ControllerTest.php
@@ -7,6 +7,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\social_api\Plugin\NetworkManager;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\social_auth\Controller\OAuth2ControllerBase;
+use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\social_auth\AuthManager\OAuth2ManagerInterface;
+use Drupal\social_auth\SocialAuthDataHandler;
+use Drupal\social_auth\User\UserAuthenticator;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 
 class ControllerTest extends UnitTestCase {
@@ -57,6 +68,34 @@ class ControllerTest extends UnitTestCase {
    */
   public function testOAuth2ControllerBase() {
     $this->assertTrue(true);
+    $messenger = $this->createMock(MessengerInterface::class);
+    $network_manager = $this->createMock(NetworkManager::class);
+    $user_authenticator = $this->createMock(UserAuthenticator::class);
+    $provider_manager = $this->createMock(OAuth2ManagerInterface::class);
+    $data_handler = $this->createMock(SocialAuthDataHandler::class);
+    $renderer = $this->createMock(RendererInterface::class);
+    $request = $this->createMock(RequestStack::class);
+    $oAuth2ControllerBase = $this->getMockBuilder(OAuth2ControllerBase::class)
+                                 ->setConstructorArgs(array('moduleName',
+                                                      'pluginId',
+                                                      $messenger,
+                                                      $network_manager,
+                                                      $user_authenticator,
+                                                      $provider_manager,
+                                                      $request,
+                                                      $data_handler,
+                                                      $renderer,
+                                                      ))
+                                 ->getMock();
+
+     $this->assertTrue(
+       method_exists($oAuth2ControllerBase, 'processCallback'),
+         'OAuth2ControllerBase does not implements processCallback function/method'
+       );
+    $this->assertTrue(
+       method_exists($oAuth2ControllerBase, 'redirectToProvider'),
+         'OAuth2ControllerBase does not implements redirectToProvider function/method'
+       );
   }
 
 }
