af0f424a7b0b4caca6bc32046868563028a19442
neerajpandey <neerajyednap@gmail.com>
2019-06-16 21:23:39 +0530

    Fixed coding standard violations in SettingsTest class

--- tests/src/Unit/SettingsTest.php
+++ tests/src/Unit/SettingsTest.php
@@ -1,33 +1,40 @@
 <?php
 
 use Drupal\Tests\UnitTestCase;
-use PHPUnit\Framework\TestCase;
 use Drupal\social_auth\Settings\SettingsBase;
-use Drupal\social_api\Settings\SettingsBase as SocialApiSettingsBase;
 use Drupal\Core\Config\ImmutableConfig;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Config\TypedConfigManagerInterface;
 
+/**
+ * Define Test Class SettingsTest.
+ */
 class SettingsTest extends UnitTestCase {
+
   /**
-   * tests for class SettingsBase
+   * Tests for class SettingsBase.
    */
-  public function testSettingsBase () {
+  public function testSettingsBase() {
     $storage = $this->createMock(StorageInterface::class);
     $event_dispatcher = $this->createMock(EventDispatcherInterface::class);
     $typed_config = $this->createMock(TypedConfigManagerInterface::class);
 
     $configs = $this->getMockBuilder(ImmutableConfig::class)
-                          ->setConstructorArgs(array('drupalConfig123', $storage, $event_dispatcher, $typed_config))
-                          ->getMock();
+      ->setConstructorArgs(['drupalConfig123',
+        $storage,
+        $event_dispatcher,
+        $typed_config,
+      ])
+      ->getMock();
 
     $collection = $this->getMockBuilder(SettingsBase::class)
-                       ->setConstructorArgs(array($configs))
-                       ->getMock();
+      ->setConstructorArgs([$configs])
+      ->getMock();
 
-    //writing this assertion, otherwise the test will throw a warning. And this test
-    //is quite important as its checking for parent methods.
+    // Writing this assertion, otherwise the test will throw a warning. And this
+    // test is quite important as its checking for parent methods.
     $this->assertTrue($collection instanceof SettingsBase);
   }
+
 }
